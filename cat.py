01d89447c4b299da07e66c4cfafc6d7af2876950
lukejwamble <wambleson3@gmail.com>
2024-02-13 10:09:20 -0600

    fixed 1
    created classes and instances

--- cat.py
+++ cat.py
@@ -1,8 +1,19 @@
 #Revmoe pass and complete the cat class
 class Cat():
-    pass
+    def __init__self():
+        self.name = "Unknown"
+        self.age = 0
 
+    def speak(self):
+        return "Meow"
 
 #Create objects here
 #These should NOT be indented inside the class
+stella = Cat()
+stella.name = "Stella"
+stella.age = 7
+
+garfield = Cat()
+garfield.name = "Garfield"
+garfield.age = 50
 
